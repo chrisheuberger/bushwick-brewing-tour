037a126228f5ac3ee9f70ce0793945c6f6054fce
Chris Heuberger <chrisheuberg@gmail.com>
2017-10-02 12:35:02 -0400

    test

--- js/main.js
+++ js/main.js
@@ -15,4 +15,36 @@ $(document).ready(function () {
         return false;
     });
 
+    // self.scrollview.pagingEnabled = NO;
+
+    $(window).scroll(function(){
+      $("#rotating-item-wrapper").css("opacity", 1 - $(window).scrollTop() / 100);
+      $(".welcome").css("opacity", 0 + $(window).scrollTop() / 100);
+    });
+
+    // http://trendmedia.com/news/infinite-rotating-images-using-jquery-javascript/
+
+    var InfiniteRotator = {
+      init: function() {
+        var initialFadeIn = 100;
+        var itemInterval = 400;
+        var fadeTime = 0;
+        var numberOfItems = $('.rotating-item').length;
+        var currentItem = 0;
+        $('.rotating-item').eq(currentItem).show(initialFadeIn);
+        var infiniteLoop = setInterval(function() {
+          $('.rotating-item').eq(currentItem).hide(fadeTime);
+          if (currentItem == numberOfItems - 1) {
+            currentItem = 0;
+          } else {
+            currentItem++;
+          }
+          $('.rotating-item').eq(currentItem).show(fadeTime);
+        }, itemInterval);
+        return false;
+      }
+    };
+
+    InfiniteRotator.init();
+
 });
